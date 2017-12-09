323d359de8e6d79ffe1fc139ee8040fe831fb15a
Kayleigh <kelyvandewiel@gmail.com>
2017-12-09 18:08:24 +0100

    Start of this project

--- CharacterCreator/Form1.Designer.cs
+++ CharacterCreator/Form1.Designer.cs
@@ -28,12 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bnext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bnext
+            // 
+            this.bnext.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.bnext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bnext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bnext.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bnext.ForeColor = System.Drawing.SystemColors.Window;
+            this.bnext.Location = new System.Drawing.Point(680, 323);
+            this.bnext.Name = "bnext";
+            this.bnext.Size = new System.Drawing.Size(162, 38);
+            this.bnext.TabIndex = 0;
+            this.bnext.Text = "Next";
+            this.bnext.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(854, 373);
+            this.Controls.Add(this.bnext);
+            this.Name = "Form1";
+            this.Text = "Character generator";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bnext;
     }
 }
 
